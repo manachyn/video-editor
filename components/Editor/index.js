035f80b2a72dff53fd373bd64ded5b6d3510e5f8
manachyn <manachyn@gmail.com>
2016-04-12 19:12:43 +0300

    To public

--- components/Editor/index.js
+++ components/Editor/index.js
@@ -1,15 +1,18 @@
+import flow from 'lodash/flow';
+import pick from 'lodash/pick';
+import mapValues from 'lodash/mapValues';
+import invariant from 'invariant';
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import mapValues from 'lodash/mapValues';
-import flow from 'lodash/flow';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import css from 'react-css-modules';
-import styles from './styles.css';
 
 import selector from '../../modules/editor/selectors';
 
+import * as layerActions from '../../modules/editor/layers/actions';
+import * as filterActions from '../../modules/editor/filters/actions';
 import * as videoActions from '../../modules/html5video/actions';
 import * as playerActions from '../../modules/player/actions';
 import * as editorActions from '../../modules/editor/actions';
@@ -18,14 +21,42 @@ import {
     videoProps,
     videoActionsShape,
     playerActionsShape,
+    filterTypeShape,
     layerShape,
 } from '../propTypes';
 
 import Player from '../Player';
 import Html5Video from '../Html5Video';
-import { Panel as FiltersPanel } from './Filters';
-import Inspector from './Inspector';
+
+import CustomDragLayer from './CustomDragLayer';
 import MainToolbar from './MainToolbar';
+import MainPanel from './MainPanel';
+import { Panel as FiltersPanel } from './Filters';
+import { Panel as LayersPanel } from './Layers';
+import Timeline from './Timeline';
+import PropertyEditor from './PropertyEditor';
+import Inspector from './Inspector';
+
+import {
+    Blur,
+    Hue,
+    Negative,
+    CSB,
+    ColorMatrix,
+} from './FilterRenderer/renderers';
+
+const FilterRenderer = require('./FilterRenderer').default;
+
+import styles from './styles';
+
+const {
+    bool,
+    number,
+    string,
+    object,
+    arrayOf,
+    shape,
+    } = PropTypes;
 
 const filterGroups = {
   behavioral: ['cut'],
@@ -39,10 +70,33 @@ const filterGroups = {
   ],
 };
 
+const filterRenderers = {
+  blur: Blur,
+  hue: Hue,
+  negative: Negative,
+  csb: CSB,
+  colorMatrix: ColorMatrix,
+};
+
+const filterProcessors = {
+  cut: (filter, api, video) => {
+    const { timeline } = filter;
+    const { currentTime } = video;
+
+    const timeStart = timeline.offset;
+    const timeEnd = timeStart + timeline.duration;
+
+    if (currentTime >= timeStart && currentTime < timeEnd) {
+      api.seek(Math.ceil(timeEnd));
+    }
+  },
+};
+
 export class Editor extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       snapToGrid: this.props.snapToGrid,
       cellSize: this.props.cellSize,
@@ -57,11 +111,30 @@ export class Editor extends Component {
       seek: offset => this.video.seek(offset),
     };
     this.refresh = this.refresh.bind(this);
+    this.handleCreateLayer = this.handleCreateLayer.bind(this);
   }
 
   refresh() {
     const { actions, video } = this.props;
     actions.editor.update(video.currentTime);
+    this.processFilters();
+  }
+
+  processFilters() {
+    const { filters, video } = this.props;
+    const behavioralFilters = Object.values(filters)
+        .filter(({ type }) => filterGroups.behavioral.indexOf(type) !== -1);
+
+    behavioralFilters.forEach(filter => {
+      const { type } = filter;
+      const processor = filterProcessors[type];
+      invariant(processor, `No filter processor is registered for type ${type}`);
+      processor(filter, this.api, video);
+    });
+  }
+
+  handleCreateLayer() {
+    this.props.actions.layer.create('effect');
   }
 
   renderVideo(size) {
@@ -69,11 +142,11 @@ export class Editor extends Component {
 
     return (
         <Html5Video ref={r => (this.video = r)}
-          preload="auto"
-          src={source}
-          onTimeUpdate={this.refresh}
-          actions={actions.video}
-          { ...{ ...size, ...video } }
+                    preload="auto"
+                    src={source}
+                    onTimeUpdate={this.refresh}
+                    actions={actions.video}
+            { ...{ ...size, ...video } }
         />
     );
   }
@@ -110,14 +183,33 @@ export class Editor extends Component {
 
     return (
         <Player {...playerProps}>
-          {videoEl}
+          {FilterRenderer ?
+              <FilterRenderer {...size}
+                  renderers={filterRenderers}
+                  filters={presentationalFilters}
+              >
+                {videoEl}
+              </FilterRenderer> :
+              videoEl
+          }
         </Player>
     );
   }
 
   render() {
-    const { className, filterTypes, video, layers, actions } = this.props;
-    const { snapToGrid, cellSize } = this.state;
+    const {
+        className,
+        filterTypes,
+        video,
+        layers,
+        actions,
+        } = this.props;
+
+    const {
+        snapToGrid,
+        cellSize,
+        } = this.state;
+
     const layersPanelProps = {
       snapToGrid,
       cellSize,
@@ -130,44 +222,56 @@ export class Editor extends Component {
         <div styleName="editor" className={className}>
           <div styleName="main">
             <FiltersPanel
-              filterTypes={filterTypes}
-              onCreateFilter={actions.editor.createFilter}
+                filterTypes={filterTypes}
+                onCreateFilter={actions.editor.createFilter}
             />
             {this.renderPlayer()}
             <Inspector />
           </div>
-          <MainToolbar onCreateLayer={this.handleCreateLayer} />
+          <MainToolbar
+              onCreateLayer={this.handleCreateLayer}
+          />
+          <MainPanel>
+            <LayersPanel {...layersPanelProps}
+                actions={pick(actions, 'layer', 'filter')}
+            >
+              <Timeline {...video} onSeek={this.api.seek} />
+            </LayersPanel>
+            <PropertyEditor />
+          </MainPanel>
+          <CustomDragLayer { ...{ snapToGrid, cellSize } } />
         </div>
     );
   }
 }
 
 Editor.propTypes = {
-  className: PropTypes.string,
+  className: string,
 
-  layerTypes: PropTypes.object.isRequired,
-  filterTypes: PropTypes.object.isRequired,
-  layers: PropTypes.arrayOf(layerShape).isRequired,
+  layerTypes: object.isRequired,
+  filterTypes: object.isRequired,
+  layers: arrayOf(layerShape).isRequired,
+  //filters: arrayOf(filterTypeShape).isRequired,
 
-  snapToGrid: PropTypes.bool,
-  cellSize: PropTypes.number,
+  snapToGrid: bool,
+  cellSize: number,
 
-  source: PropTypes.string.isRequired,
+  source: string.isRequired,
 
-  actions: PropTypes.shape({
+  actions: shape({
     video: videoActionsShape.isRequired,
     player: playerActionsShape.isRequired,
   }),
 
-  player: PropTypes.shape({
-    width: PropTypes.number,
-    height: PropTypes.number,
-    debug: PropTypes.bool,
+  player: shape({
+    width: number,
+    height: number,
+    debug: bool,
   }).isRequired,
 
-  video: PropTypes.shape(videoProps).isRequired,
+  video: shape(videoProps).isRequired,
 
-  activeFilters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  activeFilters: arrayOf(string).isRequired,
 };
 
 Editor.defaultProps = {
@@ -178,24 +282,22 @@ Editor.defaultProps = {
 const actionsMap = {
   player: playerActions,
   video: videoActions,
+  layer: layerActions,
+  filter: filterActions,
   editor: editorActions,
 };
 
-const mapDispatchToProps = dispatch => ({
+const selectActions = dispatch => ({
   actions: mapValues(
       actionsMap,
       actions => bindActionCreators(actions, dispatch)
   ),
 });
 
+/* eslint-disable new-cap */
 export default flow(
     css(styles, { allowMultiple: true }),
-    connect(selector, mapDispatchToProps)
-)(Editor);
-
-export default flow(
-    css(styles, { allowMultiple: true }),
-    connect(selector, mapDispatchToProps),
+    connect(selector, selectActions),
     DragDropContext(HTML5Backend)
 )(Editor);
-
+/* eslint-enable new-cap */
